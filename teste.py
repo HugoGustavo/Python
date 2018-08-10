8cc60e5b9da206a09038c20e422373867ab9288a
Hugo Gustavo <hugog.valin@gmail.com>
2018-08-09 22:00:26 -0300

    Variaveis

---
+++ teste.py
@@ -0,0 +1,20 @@
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+word = 'word'
+sentence = "This is a sentence"
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+
+# Primeiro comentario
+print ("Hello, Python!"); #Segundo comentario
+
+name = "Madisetti" #This is again comment
+
+#This is a comment
+#This is a comment, too.
+#This is a comment, too.
+# I said that already
+
+input("\n\nPress the enter key to exit");
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n');
